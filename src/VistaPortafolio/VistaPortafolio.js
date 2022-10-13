fa6ba81a9e23ad69eedab3600df4ecd39fb37ed7
201 <201@academicos.cesde.edu.co>
2022-10-13 14:09:11 -0500

    se trabaja rect

---
+++ src/VistaPortafolio/VistaPortafolio.js
@@ -0,0 +1,24 @@
+import "./VistaPortafolio.css"
+export function VistaPortafolio(){
+    return(
+        <>
+        
+        <section id="pantalla-divididavp">
+            <div class="tresvp">
+                <img class="imagenvp" src="https://firebasestorage.googleapis.com/v0/b/vistacomercial-2ff98.appspot.com/o/arquitectura.png?alt=media&token=d7c8a96a-2f41-411e-92af-f438d1943001" alt=""/>
+                <img class="imagenvp" src="https://firebasestorage.googleapis.com/v0/b/vistacomercial-2ff98.appspot.com/o/alimentos.jpg?alt=media&token=3f7df8d8-1718-4a54-9b98-e87b2e481314" alt=""/>
+                <img class="imagenvp" src="https://firebasestorage.googleapis.com/v0/b/vistacomercial-2ff98.appspot.com/o/producto.jpg?alt=media&token=06173332-f7e6-44c9-8128-cf3abcff9a64" alt=""/>
+                <img class="imagenvp" src="https://firebasestorage.googleapis.com/v0/b/vistacomercial-2ff98.appspot.com/o/social.jpg?alt=media&token=c3b700b2-b478-4e3c-86b3-ff6a45de3aad" alt=""/>
+            </div>
+            <div class="cuatrovp">
+                    <ul>
+                        <li class="enlacecuatrovp"><a href="" class="enlacesvp">ARQUITECTURA</a></li>
+                        <li class="enlacecincovp"><a href="" class="enlacesvp">ALIMENTOS</a></li>
+                        <li class="enlaceseisvp"><a href="" class="enlacesvp">PRODUCTO</a></li>
+                        <li class="enlacesietevp"><a href="" class="enlacesvp">SOCIAL</a></li>
+                    </ul>
+            </div>
+        </section>
+        </>
+    )
+}
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { VistaPortafolio } from './VistaPortafolio/VistaPortafolio.js';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <VistaPortafolio></VistaPortafolio>
+  </React.StrictMode>
+);
+
